01006158cd2690bfa51b80a184c264b9d854251b
邱廷鈺 Zach <zach@ocard.co>
2025-04-17 15:50:50 +0800

    feat: sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
-  modules: ["@nuxt/eslint", "@nuxtjs/i18n"],
+  modules: ["@nuxt/eslint", "@nuxtjs/i18n", "@nuxtjs/sitemap"],
   i18n: {
     defaultLocale: "zh-TW",
     locales: [
@@ -16,5 +16,9 @@ export default defineNuxtConfig({
       { code: "en", name: "English", file: "en.json" },
       { code: "th", name: "ภาษาไทย", file: "th.json" },
     ],
+  },
+  site: {
+    url: "https://zach-the-best.netlify.app",
+    name: "ZACH 好棒棒",
   },
 });
